ea1c8f6ecbb9701c6e730a07e65834da55b5af69
Nick Vigier <nvigier46@gmail.com>
2025-04-02 13:08:49 +0100

    Attempt at minimal repro - fix 1

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,11 @@
 import { redirect } from "next/navigation";
 
 export default async function SearchPage({
-  searchParams,
+	searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  redirect(`/search/results?q=${(await searchParams).q}`);
+	redirect(
+		`/search/results?q=${encodeURIComponent((await searchParams).q as string)}`,
+	);
 }
